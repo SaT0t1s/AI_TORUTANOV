9b6af89e7c146065a222b8672123ce7bfb88d8f4
SaT0t1s <166305779+SaT0t1s@users.noreply.github.com>
2024-04-07 17:44:38 +0300

    moments script

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 
 import numpy as np
 import cv2 as cv
-from picture_editing import VideoEditor
+from util.picture_edit import VideoEditor
 
 cap = cv.VideoCapture("Traffic IP Camera video (1).mp4")
 
@@ -21,11 +21,18 @@ while cap.isOpened():
         break
     frame = VideoEditor.frame_edit(frame, 1)
     mask = bg_subs.apply(frame, learningRate=learning_rate)
-    mask = VideoEditor.mask_edit(mask, 5, 1)
     out = frame.copy()
     # result = cv.bitwise_and(out, out, mask=mask)
     # cv.imshow("res", mask)
-    VideoEditor().find_ctr(mask, out)
+
+    ret, thresh1 = cv.threshold(mask, 50, 255, cv.THRESH_BINARY)
+    filter_im = VideoEditor.filter_im(thresh1, 5)
+    edited = VideoEditor.mask_edit(filter_im, 3, 3)
+    # cv.imshow("1", mask)
+    # cv.imshow("2", filter_im)
+    # cv.imshow("3", edited)
+
+    VideoEditor().find_ctr(edited, out)
     cv.imshow("input", out)
     c = cv.waitKey(10)
     if c == 27:
--- util/picture_edit.py
+++ util/picture_edit.py
@@ -13,17 +13,37 @@ class VideoEditor:
     def frame_edit(frame, scale):
         frame = cv.resize(frame, None, fx=scale, fy=scale, interpolation=cv.INTER_AREA)
         return frame
+
+    @staticmethod
+    def filter_im(frame, kernel):
+        kernel = np.ones((kernel, kernel), np.uint8)
+        opening = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+        return opening
     @staticmethod
     def mask_edit(frame, kernel, iterations):
         kernel = np.ones((kernel, kernel), np.uint8)
-        opening = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
-        # dilation = cv.dilate(frame, kernel, iterations=iterations)
-        return opening
+        dilation = cv.dilate(frame, kernel, iterations=iterations)
+        return dilation
     @staticmethod
     def find_ctr(frame, res):
         ret, thresh = cv.threshold(frame, 170, 255, 0)
-        _, im2, contoours, hierarchy = cv.findContours(
+        contours, hierarchy = cv.findContours(
             thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
         )
 
-        cv.drawContours(res, contours, -1, (0,255,0, 3))
+        for c in contours:
+            M = cv.moments(c)
+            if M["m00"] > 2500:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                cv.circle(res, (cX, cY), 10, (0, 0, 255), -1)
+
+        # moments = cv.moments(frame)
+        # dM01 = moments["m01"]
+        # dM10 = moments["m10"]
+        # darea = moments["m00"]
+        # if darea > 100:
+        #     x = int(dM10 / darea)
+        #     y = int(dM01 / darea)
+        #     cv.circle(res, (x, y), 10, (0, 0 ,255), -1)
+        cv.drawContours(res, contours, -1, (0,255,0), 3)
